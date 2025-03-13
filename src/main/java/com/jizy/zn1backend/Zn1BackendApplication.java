beafbc089fa295bb19b0f30adaa34a1442528adc
阿元 <jizuyuan@outlook.com>
2025-03-13 14:29:33 +0800

    初始化后端项目

--- src/main/java/com/jizy/zn1backend/Zn1BackendApplication.java
+++ src/main/java/com/jizy/zn1backend/Zn1BackendApplication.java
@@ -1,9 +1,14 @@
 package com.jizy.zn1backend;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.jizy.zn1backend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class Zn1BackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/jizy/zn1backend/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.jizy.zn1backend.controller;
+
+import com.jizy.zn1backend.common.BaseResponse;
+import com.jizy.zn1backend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
